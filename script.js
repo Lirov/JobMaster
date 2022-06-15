7a34fff00de496a09086cf6ce847ffd46604080f
LiroVink <lirovink@gmail.com>
2022-06-15 13:14:25 +0300

    upload JobMaster task

---
+++ script.js
@@ -0,0 +1,69 @@
+var loader = document.querySelector('.loader')
+window.addEventListener("load", vanish);
+function vanish(){
+  loader.classList.add('disappear')
+}
+
+var openTBL = document.getElementById("openTBL");
+openTBL.onclick = function () {
+  document.getElementById("tableTwo").style.display = "block";
+};
+
+function closeTBL() {
+  var closeTBL = document.getElementById("tableTwo");
+  closeTBL.style.display = "none";
+}
+
+var openTBL2 = document.getElementById("openTBL2");
+openTBL2.onclick = function () {
+  document.getElementById("tableThree").style.display = "block";
+};
+
+function closeTBL2() {
+  var closeTBL2 = document.getElementById("tableThree");
+  closeTBL2.style.display = "none";
+}
+
+var openTBL3 = document.getElementById("openTBL3");
+openTBL3.onclick = function () {
+  document.getElementById("tableFour").style.display = "block";
+};
+
+function closeTBL3() {
+  var closeTBL3 = document.getElementById("tableFour");
+  closeTBL3.style.display = "none";
+}
+
+var openTBL4 = document.getElementById("openTBL4");
+openTBL4.onclick = function () {
+  document.getElementById("tableFive").style.display = "block";
+};
+
+function closeTBL4() {
+  var closeTBL4 = document.getElementById("tableFive");
+  closeTBL4.style.display = "none";
+}
+
+var openTBL5 = document.getElementById("openTBL5");
+openTBL5.onclick = function () {
+  document.getElementById("tableSix").style.display = "block";
+};
+
+function closeTBL5() {
+  var closeTBL5 = document.getElementById("tableSix");
+  closeTBL5.style.display = "none";
+}
+
+var openTBL6 = document.getElementById("openTBL6");
+openTBL6.onclick = function () {
+  document.getElementById("tableSeven").style.display = "block";
+};
+
+function closeTBL6() {
+  var closeTBL6 = document.getElementById("tableSeven");
+  closeTBL6.style.display = "none";
+}
+
+
+
+
